f3baa467a28b85b4acfbc72ca0cdce9028442d61
Mike Rolf <mikerolfuk@gmail.com>
2015-11-05 16:49:57 +0000

    Added structure. Main class added to hold main(). Program class split into separate methods.

--- src/Program.java
+++ src/Program.java
@@ -8,39 +8,47 @@ import java.util.*;
 import java.io.*;
 
 class Program {
+    String accountSid;        
+    String authToken;        
+    String toNumber;        
+    String fromNumber;        
 
-    void run() {
+    public void run() {
+        loadProperties();
+        sendSms();
+    }
+
+    private void loadProperties() {
+        Properties properties = new Properties();
         try {
-            Properties properties = new Properties();
-            try {
-               properties.load(new FileInputStream("config.properties"));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
+           properties.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-            String accountSid = properties.getProperty("account-sid");        
-            String authToken = properties.getProperty("auth-token");        
-            String toNumber = properties.getProperty("to-number");        
-            String fromNumber = properties.getProperty("from-number");        
-    
-            TwilioRestClient client = new TwilioRestClient(accountSid, authToken);
-         
-            // Build a filter for the MessageList
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("Body", "Jenny please?! I love you <3"));
-            params.add(new BasicNameValuePair("To", toNumber));
-            params.add(new BasicNameValuePair("From", fromNumber));
-         
-            MessageFactory messageFactory = client.getAccount().getMessageFactory();
+        accountSid = properties.getProperty("account-sid");        
+        authToken = properties.getProperty("auth-token");        
+        toNumber = properties.getProperty("to-number");        
+        fromNumber = properties.getProperty("from-number");        
+    }
+
+    private void sendSms() {
+        TwilioRestClient client = new TwilioRestClient(accountSid, authToken);
+     
+        // Build a filter for the MessageList
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("Body", "Jenny please?! I love you <3"));
+        params.add(new BasicNameValuePair("To", toNumber));
+        params.add(new BasicNameValuePair("From", fromNumber));
+     
+        MessageFactory messageFactory = client.getAccount().getMessageFactory();
+
+        try {
             Message message = messageFactory.create(params);
             System.out.println(message.getSid());
         }
         catch(TwilioRestException e) {
             throw new RuntimeException(e);
         }
-    }
-    
-    public static void main(String[] args) {
-        new Program().run();
     }
 }
